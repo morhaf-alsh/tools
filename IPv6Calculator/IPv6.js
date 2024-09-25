f9997c3f4ea1cb8dc5f9eed71d1119c7b03b92e7
Morhaf <morhaf.alshoufi231@gmail.com>
2024-09-25 22:01:54 +0400

    sixth

--- IPv6Calculator/IPv6.js
+++ IPv6Calculator/IPv6.js
@@ -9,37 +9,36 @@ document.getElementById('ipv6Form').addEventListener('submit', function(event) {
   
     // Display the results
     document.getElementById('networkAddress').innerText = results.networkAddress;
-    document.getElementById('subnetMask').innerText = results.subnetMask;
     document.getElementById('firstUsable').innerText = results.firstUsable;
     document.getElementById('lastUsable').innerText = results.lastUsable;
     document.getElementById('totalAddresses').innerText = results.totalAddresses;
   
-    document.getElementById('results').classList.remove('hidden');
   });
   
   function calculateIPv6Subnet(ipv6Address, prefixLength) {
     try {
+      // the Ip is splitted with ":" then each part is filled with 0 on the left to complete the formart i.e. (20010db8000000000000000000000000)
       const ip = BigInt(`0x${ipv6Address.split(':').map(padWithZeros).join('')}`);
+      //  the network mask is  the number "1" shifted to the number of the host bits (i.e. 1000000000000000 ) then - 1 ( ffffffffffff) then shifting left
       let networkMask = (BigInt(1) << BigInt(128 - prefixLength)) - BigInt(1);
       networkMask = networkMask << BigInt(128 - prefixLength)
-
+      // the network address is AND operation between the ip address and the network mask 
       const networkAddress = (ip & networkMask);
       const firstUsable = (ip & networkMask) + BigInt(1);
-      const broadcast = ip | networkMask;
-      console.log(broadcast.toString(16))
-      const lastUsable = networkMask | ip;
+      const broadcast = networkAddress | ( networkMask >> BigInt(prefixLength) )
+      const lastUsable = broadcast - BigInt(1);
       return {
         networkAddress: formatIPv6(networkAddress.toString(16).padStart(32, '0')),
-        subnetMask: formatIPv6(networkMask.toString(16).padStart(32, '0')),
+        // subnetMask: formatIPv6(networkMask.toString(16).padStart(32, '0')),
         firstUsable: formatIPv6(firstUsable.toString(16).padStart(32, '0')),
-        lastUsable: formatIPv6((lastUsable).toString(16)),
+        lastUsable: formatIPv6((lastUsable).toString(16).padStart(32, "0")),
         totalAddresses: (BigInt(1) << BigInt(128 - prefixLength))
       };
     } catch (error) {
       console.error(error);
       return {
         networkAddress: 'Error',
-        subnetMask: 'Error',
+        // subnetMask: 'Error',
         firstUsable: 'Error',
         lastUsable: 'Error',
         totalAddresses: 'Error'
